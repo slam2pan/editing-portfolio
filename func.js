8b43ddfd0c1d371576180ee2d8d0384aef1c8ffb
slam2pan <61099400+slam2pan@users.noreply.github.com>
2020-06-08 13:43:28 -0400

    simple

---
+++ func.js
@@ -0,0 +1,13 @@
+function montageFunc() {
+	var x = document.getElementById("Montages");
+	var y = document.getElementById("IRL");
+	x.style.display = "block";
+	y.style.display = "none";
+}
+
+function IRLFunc() {
+	var x = document.getElementById("Montages");
+	var y = document.getElementById("IRL");
+	x.style.display = "none";
+	y.style.display = "block";
+}
